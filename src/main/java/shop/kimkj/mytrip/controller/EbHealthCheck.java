d6512db83ae5aa10f913bb42c2da0fa8f819cc0a
hyojin-dev <wkdgywls30@gmail.com>
2021-12-29 17:34:44 +0900

    aws 연결

--- src/main/java/shop/kimkj/mytrip/controller/EbHealthCheck.java
+++ src/main/java/shop/kimkj/mytrip/controller/EbHealthCheck.java
@@ -1,8 +1,6 @@
 package shop.kimkj.mytrip.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
